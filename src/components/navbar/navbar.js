bd6f9a4288649d174404e6cbac0eee65c6a2ea72
cgeopapa <cgeocodgod@gmail.com>
2023-06-14 19:24:17 +0300

    Embedded navbar component to main layout

--- src/components/navbar/navbar.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("hamburger")?.addEventListener('click', () => {
-    // increase the max height (max-h-16) tailwind property if need to add more menu items
-    document.getElementById('navlinks')?.classList.toggle('max-h-20');
-})
